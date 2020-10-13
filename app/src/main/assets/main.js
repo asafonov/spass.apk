07162f2f15852510401ab41dd433c95c04d194eb
asafonov <me@asafonov.org>
2020-10-13 09:00:49 +0000

    Added swipe to show/hide password

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -1,12 +1,10 @@
 class Item {
-
   constructor (name, password) {
     this.name = name;
     this.simpleAlphabet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     this.alphabet = Item.simpleAlphabet + "!#?@-*()[]\/+%:;&{},.<>_";
     this.setOrUpdate(password);
   }
-
   setOrUpdate (password) {
     if (password) {
       this.set(password);
@@ -14,65 +12,51 @@ class Item {
       this.update();
     }
   }
-
   update (simple) {
     if (simple === null || simple === undefined) {
       simple = asafonov.settings.simpleByDefault;
     }
-
     const alphabet = simple ? "simpleAlphabet" : "alphabet";
     const alphabetLength = this[alphabet].length;
     const passwordLength = asafonov.settings.passwordMinLength + parseInt((asafonov.settings.passwordMaxLength - asafonov.settings.passwordMinLength + 1) * Math.random(), 10);
     let password = '';
-
     for (let i = 0; i < passwordLength; ++i) {
       password += this[alphabet][parseInt(Math.random() * alphabetLength, 10)];
     }
-
     this.set(password);
   }
-
   set (password) {
     this.password = password;
     asafonov.messageBus.send(asafonov.events.ITEM_UPDATED, {item: this});
   }
-
   get() {
     return this.password;
   }
-
   destroy() {
   }
 }
 class List {
-
   constructor() {
     this.items = {};
     const passwords = JSON.parse(window.localStorage.getItem('passwords'));
-
     for (let i in passwords) {
       this.items[i] = new Item(i, passwords[i]);
     }
-
     asafonov.messageBus.subscribe(asafonov.events.ITEM_UPDATED, this, 'onItemUpdated');
     asafonov.messageBus.subscribe(asafonov.events.EDIT_DELETED, this, 'onEditDeleted');
     asafonov.messageBus.subscribe(asafonov.events.EDIT_SAVED, this, 'onEditSaved');
   }
-
   onItemUpdated (data) {
     this.save();
   }
-
   onEditDeleted (data) {
     const name = data.item.name;
-
     if (this.items[name]) {
       this.deleteItem(name);
       this.save();
       asafonov.messageBus.send(asafonov.events.LIST_UPDATED);
     }
   }
-
   onEditSaved (data) {
     if (data.item && data.item.name == data.name) {
       this.items[data.name].setOrUpdate(data.password);
@@ -83,13 +67,11 @@ class List {
       asafonov.messageBus.send(asafonov.events.LIST_UPDATED);
     }
   }
-
   deleteItem (name) {
     this.items[name].destroy();
     this.items[name] = null;
     delete this.items[name];
   }
-
   updateItem (name, password) {
     if (this.items[name]) {
       this.items[name].set(password);
@@ -97,30 +79,23 @@ class List {
       this.items[name] = new Item(name, password);
       asafonov.messageBus.send(asafonov.events.LIST_UPDATED);
     }
-
     this.save();
   }
-
   load (data) {
     for (let i in data) {
       this.updateItem(i, data[i]);
     }
   }
-
   asString() {
     let passwords = {};
-
     for (let i in this.items) {
       passwords[i] = this.items[i].get();
     }
-
     return JSON.stringify(passwords);
   }
-
   save() {
     window.localStorage.setItem('passwords', this.asString());
   }
-
   destroy() {
     asafonov.messageBus.unsubscribe(asafonov.events.ITEM_UPDATED, this, 'onItemUpdated');
     asafonov.messageBus.unsubscribe(asafonov.events.EDIT_DELETED, this, 'onEditDeleted');
@@ -129,11 +104,9 @@ class List {
   }
 }
 class MessageBus {
-
   constructor() {
     this.subscribers = {};
   }
-
   send (type, data) {
     if (this.subscribers[type] !== null && this.subscribers[type] !== undefined) {
       for (var i = 0; i < this.subscribers[type].length; ++i) {
@@ -141,18 +114,15 @@ class MessageBus {
       }
     }
   }
-
   subscribe (type, object, func) {
     if (this.subscribers[type] === null || this.subscribers[type] === undefined) {
       this.subscribers[type] = [];
     }
-
     this.subscribers[type].push({
       object: object,
       func: func
     });
   }
-
   unsubscribe (type, object, func) {
     for (var i = 0; i < this.subscribers[type].length; ++i) {
       if (this.subscribers[type][i].object === object && this.subscribers[type][i].func === func) {
@@ -161,17 +131,84 @@ class MessageBus {
       }
     }
   }
-
   unsubsribeType (type) {
     delete this.subscribers[type];
   }
-
   destroy() {
     for (type in this.subscribers) {
       this.unsubsribeType(type);
     }
-
     this.subscribers = null;
+  }
+}
+class Swipe {
+  constructor (element) {
+    this.x = null;
+    this.y = null;
+    this.xn = null;
+    this.yn = null;
+    this.element = element;
+    this.onTouchStartProxy = this.onTouchStart.bind(this);
+    this.onTouchMoveProxy = this.onTouchMove.bind(this);
+    this.onTouchEndProxy = this.onTouchEnd.bind(this);
+    this.addEventListeners();
+  }
+  onTouchStart (event) {
+    this.x = event.touches[0].clientX;
+    this.y = event.touches[0].clientY;
+  }
+  onTouchMove (event) {
+    this.xn = event.touches[0].clientX;
+    this.yn = event.touches[0].clientY;
+  }
+  onTouchEnd (event) {
+    const xdiff = this.x - this.xn;
+    const ydiff = this.y - this.yn;
+    this.x = null;
+    this.y = null;
+    this.xn = null;
+    this.yn = null;
+    if (Math.abs(xdiff) > Math.abs(ydiff)) {
+      this[xdiff < 0 ? 'onRight' : 'onLeft']();
+    } else {
+      this[ydiff < 0 ? 'onDown' : 'onUp']();
+    }
+  }
+  onLeft (f) {
+    f && (this.onLeft = f);
+    return this;
+  }
+  onRight (f) {
+    f && (this.onRight = f);
+    return this;
+  }
+  onUp (f) {
+    f && (this.onUp = f);
+    return this;
+  }
+  onDown (f) {
+    f && (this.onDown = f);
+    return this;
+  }
+  manageEventListeners (remove) {
+    const action = remove ? 'removeEventListener' : 'addEventListener';
+    this.element[action]('touchstart', this.onTouchStartProxy);
+    this.element[action]('touchmove', this.onTouchMoveProxy);
+    this.element[action]('touchend', this.onTouchEndProxy);
+  }
+  addEventListeners() {
+    this.manageEventListeners();
+  }
+  removeEventListeners() {
+    this.manageEventListeners(true);
+  }
+  destroy() {
+    this.x = null;
+    this.y = null;
+    this.xn = null;
+    this.yn = null;
+    this.removeEventListeners();
+    this.element = null;
   }
 }
 class BackupView {
@@ -186,7 +223,6 @@ class BackupView {
     this.promptCancelledProxy = this.promptCancelled.bind(this);
     this.manageEventListeners();
   }
-
   manageEventListeners (remove) {
     const action = remove ? 'removeEventListener' : 'addEventListener';
     this.element[action]('click', this.onPopupProxy);
@@ -195,47 +231,37 @@ class BackupView {
     this.prompt.querySelector('.ok')[action]('click', this.promptAcceptedProxy);
     this.prompt.querySelector('.cancel')[action]('click', this.promptCancelledProxy);
   }
-
   onPopup() {
     this.element.classList.add('open');
   }
-
   enterHostnameDialog() {
     this.prompt.classList.remove('hidden');
     asafonov.messageBus.send(asafonov.events.POPUP_SHOW);
   }
-
   closeHostnameDialog() {
     this.prompt.classList.add('hidden');
     asafonov.messageBus.send(asafonov.events.POPUP_HIDE);
   }
-
   promptAccepted() {
     const hostname = this.prompt.querySelector('input[name=hostname]').value;
-
     if (! hostname) {
       alert("Hostname can't be empty");
       return;
     }
-
     this[this.promptAction](hostname);
     this.closeHostnameDialog();
   }
-
   promptCancelled() {
     this.closeHostnameDialog();
   }
-
   onFileExport() {
     this.promptAction = 'exportFile';
     this.enterHostnameDialog();
   }
-
   onFileImport() {
     this.promptAction = 'importFile';
     this.enterHostnameDialog();
   }
-
   importFile (hostname) {
     fetch('http://' + hostname + ':9092/data/')
     .then(response => response.json())
@@ -246,7 +272,6 @@ class BackupView {
       alert(error.message);
     });
   }
-
   exportFile (hostname) {
     fetch('http://' + hostname + ':9092/post/', {
       method: 'POST',
@@ -262,11 +287,9 @@ class BackupView {
       alert(error.message);
     });
   }
-
   hidePopup() {
     this.element.classList.remove('open');
   }
-
   destroy() {
     this.manageEventListeners(true);
     this.element = null;
@@ -274,7 +297,6 @@ class BackupView {
   }
 }
 class ItemView {
-
   constructor() {
     this.element = document.querySelector('.editItem');
     this.cancelButton = document.querySelector('.cancel');
@@ -291,7 +313,6 @@ class ItemView {
     this.newButton.addEventListener('click', this.onNewProxy);
     asafonov.messageBus.subscribe(asafonov.events.EDIT_STARTED, this, 'onEditStarted');
   }
-
   show() {
     this.render();
     this.element.classList.remove('hidden');
@@ -300,7 +321,6 @@ class ItemView {
     document.querySelector('.new_item_ico').classList.add('hidden');
     asafonov.messageBus.send(asafonov.events.POPUP_SHOW);
   }
-
   hide() {
     this.element.classList.add('hidden');
     this.cancelButton.classList.add('hidden');
@@ -308,45 +328,37 @@ class ItemView {
     document.querySelector('.new_item_ico').classList.remove('hidden');
     asafonov.messageBus.send(asafonov.events.POPUP_HIDE);
   }
-
   render () {
     this.element.querySelector('.name').innerHTML = (this.model ? 'Edit' : 'Add') + ' Spass Item';
     this.element.querySelector('input[name=item_name]').value = this.model ? this.model.name : '';
     this.deleteButton.classList[this.model ? 'remove' : 'add']('hidden');
   }
-
   onEditStarted (data) {
     this.model = data.item;
     this.show();
   }
-
   onCancel() {
     this.hide();
     asafonov.messageBus.send(asafonov.events.EDIT_CANCELLED);
   }
-
   onDelete() {
     this.hide();
     asafonov.messageBus.send(asafonov.events.EDIT_DELETED, {item: this.model});
   }
-
   onSave() {
     const genpass = this.element.querySelector('#genpass').checked;
     const name = this.element.querySelector('input[name=item_name]');
     const pass = this.element.querySelector('input[name=item_man_pass]');
     name.classList.remove('error');
     pass.classList.remove('error');
-
     if (name.value == '') {
       name.classList.add('error');
       return;
     }
-
     if (! genpass && pass.value == '') {
       pass.classList.add('error');
       return;
     }
-
     this.hide();
     asafonov.messageBus.send(asafonov.events.EDIT_SAVED, {
       item: this.model,
@@ -354,11 +366,9 @@ class ItemView {
       password: genpass ? '' : pass.value
     });
   }
-
   onNew() {
     asafonov.messageBus.send(asafonov.events.EDIT_STARTED, {});
   }
-
   destroy() {
     this.cancelButton.removeEventListener('click', this.hideProxy);
     this.saveButton.removeEventListener('click', this.onSaveProxy);
@@ -372,7 +382,6 @@ class ItemView {
   }
 }
 class ItemListView {
-
   constructor (model) {
     this.template = document.querySelector('.templates .itemlist').innerHTML;
     this.model = model;
@@ -383,8 +392,12 @@ class ItemListView {
     this.onGenerateProxy = this.onGenerate.bind(this);
     this.onEditProxy = this.onEdit.bind(this);
     this.hideAllDonesProxy = this.hideAllDones.bind(this);
+    this.showPassProxy = this.showPass.bind(this);
+    this.hidePassProxy = this.hidePass.bind(this);
+    this.swiper = new Swipe(this.element);
+    this.swiper.onLeft(this.showPassProxy);
+    this.swiper.onRight(this.hidePassProxy);
   }
-
   manageEventListeners (remove) {
     const action = remove ? 'removeEventListener' : 'addEventListener';
     this.element[action]('click', this.onClickProxy);
@@ -392,68 +405,63 @@ class ItemListView {
     this.element.querySelector('.generate')[action]('click', this.onGenerateProxy);
     this.element.querySelector('.edit')[action]('click', this.onEditProxy);
   }
-
   render() {
     this.element.innerHTML = this.template.replace(/{name}/g, this.model.name).replace(/{value}/g, this.model.get());
     this.manageEventListeners();
     return this.element;
   }
-
+  showPass() {
+    this.element.querySelector('.name').innerHTML = this.model.get();
+  }
+  hidePass() {
+    this.element.querySelector('.name').innerHTML = this.model.name;
+  }
   hideAllActions() {
     const actions = document.querySelectorAll('.open');
-
     for (let i = 0; i < actions.length; ++i) {
       actions[i].classList.remove('open');
     }
   }
-
   hideAllDones() {
     const dones = this.element.querySelectorAll('.true');
-
     for (let i = 0; i < dones.length; ++i) {
       dones[i].classList.remove('true');
     }
   }
-
   onCopy() {
     navigator.clipboard.writeText(this.model.get()).then(() => {
       console.log("success");
     }).catch((e)=> {
       console.log(e);
     });
-
     if (NativeAndroid !== null && NativeAndroid !== undefined) {
       NativeAndroid.copyToClipboard(this.model.get());
     }
-
     this.element.querySelector('.copy .done').classList.add('true');
     setTimeout(this.hideAllDonesProxy, 2000);
   }
-
   onGenerate() {
     this.model.update();
     this.element.querySelector('.generate .done').classList.add('true');
     setTimeout(this.hideAllDonesProxy, 2000);
   }
-
   onEdit() {
     asafonov.messageBus.send(asafonov.events.EDIT_STARTED, {item: this.model});
   }
-
   onClick (event) {
     this.hideAllActions();
     this.element.classList.add('open');
   }
-
   destroy() {
     this.manageEventListeners(true);
+    this.swiper.destroy();
+    this.swiper = null;
     this.template = null;
     this.model = null;
     this.element = null;
   }
 }
 class ListView {
-
   constructor (model) {
     this.model = model;
     this.views = {};
@@ -462,32 +470,25 @@ class ListView {
     asafonov.messageBus.subscribe(asafonov.events.POPUP_SHOW, this, 'hide');
     asafonov.messageBus.subscribe(asafonov.events.POPUP_HIDE, this, 'show');
   }
-
   render() {
     this.element.innerHTML = '';
-
     for (let i in this.model.items) {
       this.views[i] || (this.views[i] = new ItemListView(this.model.items[i]));
       this.element.appendChild(this.views[i].render());
     }
-
     this.show();
   }
-
   show() {
     this.element.classList.remove('hidden');
   }
-
   hide() {
     this.element.classList.add('hidden');
   }
-
   destroy() {
     for (let i in this.views) {
       this.views.destroy();
       this.views[i] = null;
     }
-
     this.views = null;
     this.model = null;
     this.element = null;
