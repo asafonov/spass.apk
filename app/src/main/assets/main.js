c84ce370d205db904aa00c9450adbb5eaabe5027
asafonov <me@asafonov.org>
2020-10-14 10:43:09 +0000

    minor improvements for swiper

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -142,11 +142,12 @@ class MessageBus {
   }
 }
 class Swipe {
-  constructor (element) {
+  constructor (element, minMovement) {
     this.x = null;
     this.y = null;
     this.xn = null;
     this.yn = null;
+    this.minMovement = minMovement || 100;
     this.element = element;
     this.onTouchStartProxy = this.onTouchStart.bind(this);
     this.onTouchMoveProxy = this.onTouchMove.bind(this);
@@ -168,6 +169,9 @@ class Swipe {
     this.y = null;
     this.xn = null;
     this.yn = null;
+    if (Math.abs(xdiff) < this.minMovement && Math.abs(ydiff) < this.minMovement) {
+      return ;
+    }
     if (Math.abs(xdiff) > Math.abs(ydiff)) {
       this[xdiff < 0 ? 'onRight' : 'onLeft']();
     } else {
@@ -207,6 +211,7 @@ class Swipe {
     this.y = null;
     this.xn = null;
     this.yn = null;
+    this.minMovement = null;
     this.removeEventListeners();
     this.element = null;
   }
